ca8323209b105b0b205f4c9fd43e767daea40187
Myxolidian <88684755+Myxolidian@users.noreply.github.com>
2022-11-02 21:52:49 -0300

    agrego clase DetalleInsumo - DetallePlato - DetalleBebida

---
+++ Dominio/DetalleInsumo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class DetalleInsumo
+    {
+        public int ID { get; set; }
+        public float PrecioUnitario { get; set; }
+        public int Cantidad { get; set; }
+        public float PrecioTotal { get; set; }
+        public bool Activo { get; set; }
+    }
+}
