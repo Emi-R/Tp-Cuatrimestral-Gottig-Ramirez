d661d492b8b0397788be78cc4903941951ab36bc
marie177 <gottigmarianella@hotmail.com>
2022-11-02 21:27:41 -0300

    Agrego clase Usuario!!

---
+++ Dominio/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Usuario
+    {
+        public int Legajo { get; set; }
+        public string Password { get; set; }
+        public int Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        //public Domicilio Domicilio { get; set; }
+        //public Perfil Perfil { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public bool Activo { get; set; }
+    }
+}
