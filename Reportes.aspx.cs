05bd92f331302a4965d536f3fe3153305a5bca56
marie177 <gottigmarianella@hotmail.com>
2022-12-14 17:53:08 -0300

    Avances en mostrar reportes

--- Reportes.aspx.cs
+++ Reportes.aspx.cs
@@ -11,23 +11,63 @@ namespace TP_Cuatrimestral
 {
     public partial class Reportes : System.Web.UI.Page
     {
+        private ReporteNegocio negocioReporte;
         protected void Page_Load(object sender, EventArgs e)
         {
-            CargarRepeaterReporteMeseros();
+            negocioReporte = new ReporteNegocio();
+
+            if(!IsPostBack){
+                CargarRepeaterReporteMeseros();
+                CargarRepeaterReporteMesas();
+                CargarDatosDiarios();
+            }
         }
 
-        private void CargarRepeaterReporteMeseros()
+        private void CargarRepeaterReporteMeseros(string fechaFiltro = "")
         {
-            ReporteNegocio negocioReporte = new ReporteNegocio();
-            List<ReporteMeseros> listaReporteMeseros = negocioReporte.ObtenerReporteMeseros(DateTime.Now.ToString("yyyy/MM/dd"));
+            List<ReporteMeseros> listaReporteMeseros;
+            if (string.IsNullOrEmpty(fechaFiltro))
+                listaReporteMeseros = negocioReporte.ObtenerReporteMeseros(DateTime.Now.ToString("yyyy/MM/dd"));
+            else
+                listaReporteMeseros = negocioReporte.ObtenerReporteMeseros(fechaFiltro);
 
             repeaterReporteMeseros.DataSource = listaReporteMeseros;
             repeaterReporteMeseros.DataBind();
+        }
 
-            var total = listaReporteMeseros.Sum(x => x.TotalRecaudado);
+        public void CargarDatosDiarios(string fechaFiltro = "")
+        {
+            List<ReporteMeseros> listaReporteMeseros;
+            if (string.IsNullOrEmpty(fechaFiltro))
+                listaReporteMeseros = negocioReporte.ObtenerReporteMeseros(DateTime.Now.ToString("yyyy/MM/dd"));
+            else
+                listaReporteMeseros = negocioReporte.ObtenerReporteMeseros(fechaFiltro);
 
-            lblTotalRecaudado.Text = $"{lblTotalRecaudado.Text} {total}";
 
+            var totalDiario = listaReporteMeseros.Sum(x => x.TotalRecaudado);
+            lblTotalRecaudado.Text = $"$ {totalDiario}";
+
+            //txtFechaReportes.Text = DateTime.Now.ToString("yyyy-MM-dd");
+        }
+
+        private void CargarRepeaterReporteMesas(string fechaFiltro = "")
+        {
+            List<ReporteMesas> listaReporteMesas;
+            if (string.IsNullOrEmpty(fechaFiltro))
+                listaReporteMesas = negocioReporte.ObtenerReporteMesas(DateTime.Now.ToString("yyyy/MM/dd"));
+            else
+                listaReporteMesas = negocioReporte.ObtenerReporteMesas(fechaFiltro);
+
+            repeaterReporteMesas.DataSource = listaReporteMesas;
+            repeaterReporteMesas.DataBind();
+        }
+
+        protected void txtFechaReportes_TextChanged(object sender, EventArgs e)
+        {
+            string fechaFiltro = (Convert.ToDateTime(txtFechaReportes.Text)).ToString("yyyy/MM/dd");
+            CargarRepeaterReporteMesas(fechaFiltro);
+            CargarRepeaterReporteMeseros(fechaFiltro);
+            CargarDatosDiarios(fechaFiltro);
         }
     }
 }
--- Reportes.aspx.designer.cs
+++ Reportes.aspx.designer.cs
@@ -15,6 +15,33 @@ namespace TP_Cuatrimestral
     {
 
         /// <summary>
+        /// Control updatePanel.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel updatePanel;
+
+        /// <summary>
+        /// Control lblTotalRecaudadoTitle.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTotalRecaudadoTitle;
+
+        /// <summary>
+        /// Control lblFechaTitle.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblFechaTitle;
+
+        /// <summary>
         /// Control lblTotalRecaudado.
         /// </summary>
         /// <remarks>
@@ -24,6 +51,24 @@ namespace TP_Cuatrimestral
         protected global::System.Web.UI.WebControls.Label lblTotalRecaudado;
 
         /// <summary>
+        /// Control txtFechaReportes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFechaReportes;
+
+        /// <summary>
+        /// Control lblPedidosMeseroTitle.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblPedidosMeseroTitle;
+
+        /// <summary>
         /// Control repeaterReporteMeseros.
         /// </summary>
         /// <remarks>
@@ -31,5 +76,23 @@ namespace TP_Cuatrimestral
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Repeater repeaterReporteMeseros;
+
+        /// <summary>
+        /// Control lblReporteMesasTitle.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblReporteMesasTitle;
+
+        /// <summary>
+        /// Control repeaterReporteMesas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater repeaterReporteMesas;
     }
 }
